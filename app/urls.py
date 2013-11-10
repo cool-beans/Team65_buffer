a7331c4264b6a139cfc38d437929455045672671
Lauryn Brown <laurynb@andrew.cmu.edu>
2013-11-10 15:27:58 -0500

    Created the members page.

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,20 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'app.views.home'),
+    url(r'^$', 'app.views.home', name='home'),
+   #############################################################
+   # Main Navigation
+   #############################################################
+    url(r'^about/$', 'app.views.about', name='about'),
+    url(r'^programs/$', 'app.views.programs', name='programs'),
+    #log in
+   url(r'^login/$', 'django.contrib.auth.views.login', {'template_name':'app/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
+    url(r'^register/$', 'app.views.register', name='register'),
+   # Programs
+    url(r'^program_profile/$', 'app.views.program_profile', name='program_profile'),#should be the individual program
+
+   # Members
+    url(r'^members/$', 'app.views.members', name='members'),
+    url(r'^member_profile/$', 'app.views.member_profile', name='member_profile'),
 )
--- app/views.py
+++ app/views.py
@@ -9,3 +9,31 @@ from app.models import *
 def home(request):
     context = {}
     return render(request, 'final_project/index.html', context)
+
+def programs(request):
+    context = {}
+    return render(request, 'final_project/programs.html', context)
+
+def program_profile(request):
+    context = {}
+    return render(request, 'final_project/program_profile.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'final_project/about.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'final_project/login.html', context)
+
+def register(request):
+    context = {}
+    return render(request, 'final_project/register.html', context)
+
+def members(request):
+    context = {}
+    return render(request, 'final_project/members.html', context)
+
+def member_profile(request):
+    context = {}
+    return render(request, 'final_project/member_profile.html', context)
