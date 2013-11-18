189f229afd3b81cd861c333d57d37d5e9b914594
Rauhit Ashar <rashar@andrew.cmu.edu>
2013-11-17 21:19:02 -0500

    Fixed syntax errors in forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 from django.db import models
 from django.forms import ModelForm
+from app.models import *
 
-
-class ProgramCreation(ModelForm):
+class ProgramCreation(forms.ModelForm):
     class Meta:
-        model = Article
+        model = Program
         fields = ['name','description']
 
 
-class ProgramMod(Form):
+class ProgramMod(forms.Form):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=500)
 
