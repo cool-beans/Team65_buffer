baf5aff21e288e3aa25a980fd61cb59ff58c904f
Rauhit Ashar <rashar@andrew.cmu.edu>
2013-12-02 21:33:47 -0500

    Created a module for all of the actions.

--- project/urls.py
+++ project/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^final_project/', include('app.urls')),
-    url(r'^$', 'app.views-common.home', name='home'),
+    url(r'^$', 'app.views.common.home', name='home'),
 )
