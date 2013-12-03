2c1a49c2572e0735d3008c5812838ee603da453b
Lauryn Brown <laurynb@andrew.cmu.edu>
2013-12-02 23:10:34 -0500

    Got they styles working

---
+++ app/static/static/js/application.js
@@ -0,0 +1,44 @@
+$(window).bind("load", function() {
+
+       var footerHeight = 0,
+           footerTop = 0,
+           $footer = $("#footer");
+
+       positionFooter();
+
+       function positionFooter() {
+
+                footerHeight = $footer.height();
+                footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+
+               if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+                   $footer.css({
+                        position: "absolute"
+                   }).animate({
+                        top: footerTop
+                   })
+               } else {
+                   $footer.css({
+                        position: "static"
+                   })
+               }
+
+       }
+
+       $(window)
+               .scroll(positionFooter)
+               .resize(positionFooter)
+
+});
+/* Homepage */
+//script to initalize the carousel
+$(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 2000
+   });
+/* Navigation */
+//script to make the navigationbar sticky
+ $(window).load(function(){
+      $("#nav-container").sticky({ topSpacing: 0 });
+   });
+});
